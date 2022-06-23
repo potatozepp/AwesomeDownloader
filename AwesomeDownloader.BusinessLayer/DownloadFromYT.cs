5beae60abc02d8d56aa117b0924154f3f3e0eee3
amhplang <angelo.haberz@langbau.at>
2022-06-23 14:38:20 +0200

    Downloads added

--- AwesomeDownloader.BusinessLayer/DownloadFromYT.cs
+++ AwesomeDownloader.BusinessLayer/DownloadFromYT.cs
@@ -3,11 +3,11 @@ using YoutubeExplode.Videos.Streams;
 using AwesomeDownloader.Models;
 
 namespace AwesomeDownloader.BusinessLayer {
-    static public class DownloadFromYT {
-        static public async void DownloadMP3Async(string path, string url) {
+    public class DownloadFromYT {
+        public async void DownloadMP3Async(string path, string url) {
             var youtube = new YoutubeClient();
 
-            // You can specify both video ID or URL https://www.youtube.com/watch?v=DjbVh4i0DIU
+            // You can specify both video ID or URL https://www.youtube.com/watch?v=hhxjSaF0Ek4
             var video = await youtube.Videos.GetAsync(url);
 
             var test = await youtube.Videos.Streams.GetManifestAsync(video.Id);
@@ -15,9 +15,25 @@ namespace AwesomeDownloader.BusinessLayer {
 
             var streamInfo = test.GetAudioOnlyStreams().GetWithHighestBitrate();
 
-            await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{path}\\{video.Title}.mp3");//comment
+            await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{path}\\{video.Title}.mp3");
         }
 
+        public async void DownloadMP4Async(string path, string url) {
+            var youtube = new YoutubeClient();
+
+            var video = await youtube.Videos.GetAsync(url);
+
+            var test = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+
+            var streamInfo = test.GetMuxedStreams().GetWithHighestBitrate();
+
+            await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{path}\\{video.Title}.mp4");
+        }
+
+        public async void DownloadMP3AndMP4Async(string path, string url) {
+            DownloadMP3Async(path, url);
+            DownloadMP4Async(path, url);
+        }
 
     }
 }
--- AwesomeDownloader/DownloadPage.xaml.cs
+++ AwesomeDownloader/DownloadPage.xaml.cs
@@ -22,7 +22,22 @@ namespace AwesomeDownloader.View {
         List<MP3Data> Songs { get; set; } = new List<MP3Data>();
 
         private void Button_Download_Click(object sender, RoutedEventArgs e) {
-            DownloadFromYT.DownloadMP3Async(DownloadFolder, TextBox_URL.Text);
+            DownloadFromYT downloadFromYT = new DownloadFromYT();
+
+            switch(ComboBox_FileType.SelectedIndex) {
+                case 0:
+                    downloadFromYT.DownloadMP3Async(DownloadFolder, TextBox_URL.Text);
+                    break;
+                case 1:
+                    downloadFromYT.DownloadMP4Async(DownloadFolder, TextBox_URL.Text);
+                    break;
+                case 2:
+                    downloadFromYT.DownloadMP3AndMP4Async(DownloadFolder, TextBox_URL.Text);
+                    break;
+                default:
+                    break;
+            }
+            .DownloadMP3Async(DownloadFolder, TextBox_URL.Text);
        
         }
 
